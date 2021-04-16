b27d534c254d866386c5b43cbb7104fc5258e384
Antony Konstantinidis <ak@digitalessence.de>
2021-04-16 17:34:48 +0200

    create a navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <AppNavigation />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import AppNavigation from "@/components/AppNavigation/AppNavigation.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    AppNavigation,
   },
 });
 </script>
@@ -22,6 +21,5 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
