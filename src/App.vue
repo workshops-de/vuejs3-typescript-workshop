dde48b72946b55a375f6eb051155a968f8d92190
Antony Konstantinidis <ak@digitalessence.de>
2021-04-16 17:37:56 +0200

    Create a MouseCursor component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <AppNavigation />
+  <MouseCursor />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import AppNavigation from "@/components/AppNavigation/AppNavigation.vue";
+import MouseCursor from "@/components/MouseCursor/MouseCursor.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     AppNavigation,
+    MouseCursor,
   },
 });
 </script>
---
+++ src/components/MouseCursor/MouseCursor.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-bind:id="id">x: {{ x }}, y: {{ y }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface ComponentData {
+  id: string;
+  x: number;
+  y: number;
+}
+
+export default defineComponent({
+  data(): ComponentData {
+    return {
+      id: "my-id",
+      x: 12,
+      y: 4,
+    };
+  },
+});
+</script>
